5047c56f834cc355b86b72a2b5aa608c472234d8
CharlesOrdonio <119359066+CharlesOrdonio@users.noreply.github.com>
2023-12-14 20:27:27 +0000

    Terminar o parciona

--- src/main/ordenadores/OrdenarPorContagemSimples.java
+++ src/main/ordenadores/OrdenarPorContagemSimples.java
@@ -4,6 +4,27 @@ public class OrdenarPorContagemSimples implements Ordenador{
 
     @Override
     public int[] ordene(int[] array) {
+        int max = array[0];
+        int x = 0;
+        while(x < array.length){
+            if(max < array[x + 1]){
+                max = array[x + 1];
+            }
+            x = x + 1;
+        }
+
+        int[] lista;
+        lista = new int[max];
+        for(int i = 0; i < array.length; i++){
+            lista[array[i]] = array[i];
+        }
+
+        for(int j = 0; j < lista.length; j++){
+            if(lista[j] == j){
+                System.out.println(j);
+
+            }
+        }
 
         return array;
     }
